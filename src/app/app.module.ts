d49fa31f9167dd94b72986f6adcadf85b59f2a86
Cristian Lemus <cristiandlemus1@gmail.com>
2023-02-26 20:38:30 -0500

    add spinner loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModuleModule } from './shared-module/shared-module.module';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { LoadingInterceptor } from './services/loading.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SpinnerComponent
   ],
   imports: [
     HttpClientModule,
@@ -20,7 +23,9 @@ import { SharedModuleModule } from './shared-module/shared-module.module';
     ReactiveFormsModule,
     SharedModuleModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
